ce966f450476b0fa39fdbb9dcf2e51c116c4451d
Daniel Vilela <dvilelaportela@gmail.com>
2020-04-17 03:34:08 -0300

    Full game - Js complete

--- js/jogo.js
+++ js/jogo.js
@@ -6,6 +6,7 @@ function iniciarJogo() {
     
     var nivel_jogo = url.replace("?", "")
     
+
     var tempo_segundos = 0;
 
     if(nivel_jogo == 1) { //1 Fácil -> 120 segundos
@@ -38,6 +39,7 @@ function iniciarJogo() {
     document.getElementById('baloes_estourados').innerHTML = 0;
 
     contagem_tempo(tempo_segundos + 1);
+
 }
 
 
@@ -67,7 +69,47 @@ function cria_baloes(qtde_baloes) {
         var balao = document.createElement("img");
         balao.src = 'img/balao_azul_pequeno.png';
         balao.style.margin = '10px';
+        balao.id = 'b' +i;
+        balao.onclick = function () { estourar(this); };
 
         document.getElementById('cenario').appendChild(balao);
     }
+}
+
+function estourar (e) {
+
+    var id_balao = e.id;
+
+    document.getElementById(id_balao).setAttribute ("onclick","")
+    document.getElementById(id_balao). src = 'img/balao_azul_pequeno_estourado.png';
+
+    pontuacao(-1);
+}
+
+function pontuacao(acao) {
+    var baloes_inteiros = document.getElementById('baloes_inteiros').innerHTML;
+    var baloes_estourados = document.getElementById('baloes_estourados').innerHTML;
+
+    baloes_inteiros = parseInt(baloes_inteiros);
+    baloes_estourados = parseInt(baloes_estourados);
+
+    baloes_inteiros = baloes_inteiros + acao;
+    baloes_estourados = baloes_estourados - acao;
+
+    document.getElementById('baloes_inteiros').innerHTML = baloes_inteiros;
+    document.getElementById('baloes_estourados').innerHTML = baloes_estourados;
+
+    condicao_jogo (baloes_inteiros, baloes_estourados);
+}
+
+function condicao_jogo (baloes_inteiros) {
+    if(baloes_inteiros == 0) {
+        alert('Congratulations, you are the winner! He managed to pop all the balloons in time! ;)')
+        parar_jogo();
+    }
+}
+
+function parar_jogo (){
+    clearTimeout(timerId);
+
 }
